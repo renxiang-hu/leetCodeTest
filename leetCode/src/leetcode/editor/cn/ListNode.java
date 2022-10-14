64b8a2cae6453eb1a5eb4c0eb1a339571c177fe9
胡仁祥 <18917734365@163.com>
2022-10-14 10:50:43 +0800

    合并两个有序列表

--- leetCode/src/leetcode/editor/cn/ListNode.java
+++ leetCode/src/leetcode/editor/cn/ListNode.java
@@ -6,4 +6,7 @@ public class ListNode {
     ListNode(int x){
         val = x;
     }
+    ListNode(){
+
+    }
 }
--- leetCode/src/leetcode/editor/cn/mergeTwoSortedLists.java
+++ leetCode/src/leetcode/editor/cn/mergeTwoSortedLists.java
@@ -63,17 +63,37 @@ class mergeTwoSortedLists{
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null){
-            return list2;
-        } else if (list2 == null){
-            return list1;
-        } else if (list1.val < list2.val){
-            list1.next = mergeTwoLists(list1.next,list2);
-            return list1;
-        } else {
-            list2.next = mergeTwoLists(list1,list2.next);
-            return list2;
+        //方法一：递归
+//        if (list1 == null){
+//            return list2;
+//        } else if (list2 == null){
+//            return list1;
+//        } else if (list1.val < list2.val){
+//            list1.next = mergeTwoLists(list1.next,list2);
+//            return list1;
+//        } else {
+//            list2.next = mergeTwoLists(list1,list2.next);
+//            return list2;
+//        }
+        //方法二：逐个查找比较
+        ListNode dummyNode = new ListNode();
+        ListNode cur = dummyNode;
+        while(list1 != null && list2 != null){
+            if (list1.val > list2.val){
+                cur.next = list2;
+                list2 = list2.next;
+            } else {
+                cur.next = list1;
+                list1 = list1.next;
+            }
+            cur = cur.next;
         }
+        if (list1 == null){
+            cur.next = list2;
+        } else {
+            cur.next = list1;
+        }
+        return dummyNode.next;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
