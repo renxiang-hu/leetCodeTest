b968db2693638135226650b74588b3681d587cfd
胡仁祥 <18917734365@163.com>
2023-02-10 15:10:25 +0800

    搜索插入位置

--- leetCode/src/leetcode/editor/cn/SearchInsertPosition.java
+++ leetCode/src/leetcode/editor/cn/SearchInsertPosition.java
@@ -56,10 +56,10 @@ class Solution {
         int right = nums.length - 1;
         while (left <= right){
             int middle = left + (right - left) / 2;
-            if (nums[middle] < target){
-                left = middle + 1;
-            } else if(nums[middle] > target){
+            if (nums[middle] > target){
                 right = middle - 1;
+            } else if (nums[middle] < target){
+                left = middle + 1;
             } else {
                 return middle;
             }
