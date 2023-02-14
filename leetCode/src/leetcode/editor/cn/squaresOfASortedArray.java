6fdd36aec6ffdf78c5ab7389386b5ea7d7bf1c09
胡仁祥 <18917734365@163.com>
2023-02-14 09:52:03 +0800

    加一

---
+++ leetCode/src/leetcode/editor/cn/squaresOfASortedArray.java
@@ -0,0 +1,69 @@
+//给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。 
+//
+// 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [-4,-1,0,3,10]
+//输出：[0,1,9,16,100]
+//解释：平方后，数组变为 [16,1,0,9,100]
+//排序后，数组变为 [0,1,9,16,100] 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [-7,-3,2,3,11]
+//输出：[4,9,9,49,121]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// nums 已按 非递减顺序 排序 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 请你设计时间复杂度为 O(n) 的算法解决本问题 
+// 
+//
+// Related Topics 数组 双指针 排序 👍 718 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+
+class squaresOfASortedArray{
+    //2023-02-13 11:49:46
+    //有序数组的平方
+    //编号：[977]
+    
+    public static void main(String[] args) {
+        Solution solution = new squaresOfASortedArray().new Solution();
+        int[] ints = solution.sortedSquares(new int[]{-4, -1, 0, 3, 10});
+        System.out.println(ints.toString());
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0 ; i < nums.length ; i++){
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
